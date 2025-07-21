ac7044d625a76b26c88f603b72347db59f5a2b0e
saulmrto <197935776+saulmrto@users.noreply.github.com>
2025-07-21 12:48:18 -0600

    Refactor hint logic in number guessing game
    Simplified and corrected the calculation of hint ranges for guesses close to the secret number. Removed debug print statements for cleaner output.

--- aen.py
+++ aen.py
@@ -9,28 +9,17 @@ numero_inicial = 1
 numero_final = 100
 
 numero_secreto = random.randint(numero_inicial, numero_final)
-print(numero_secreto)
-print(int(numero_final * .10 + numero_secreto))
-print(int(numero_final * .10 - numero_secreto))
 
 while True:
     numero_seleccionado = input(f"¿En que numero estoy pensando del {numero_inicial} al {numero_final}? ")
     if int(numero_seleccionado) == int(numero_secreto):
         print(f"¡Felicidades, le atinaste, era el numero {numero_secreto}!")
         break
-    elif int((numero_seleccionado)) > int((numero_secreto)) and int(numero_seleccionado) < int(numero_final * .10 + numero_secreto):
+    elif int((numero_seleccionado)) > int((numero_secreto)) and int(numero_seleccionado) < int(numero_secreto + (numero_final * .10)):
         print("Deberias pisar la tierra un poco mas de cerca.")
-    elif int((numero_seleccionado)) < int((numero_secreto)) and int(numero_seleccionado) > int(numero_final * .10 - numero_secreto):
+    elif int((numero_seleccionado)) < int((numero_secreto)) and int(numero_seleccionado) > int(numero_secreto - (numero_final * .10)):
         print("Deberias pisar el cielo un poco mas de cerca.")
     elif int((numero_seleccionado)) > int((numero_secreto)):
          print("Sientes que tocas las nubes.")
     elif int((numero_seleccionado)) < int((numero_secreto)):
         print("¿Esto es arena movediza?.")
-
-
-
-"""
-    else:
-        print(f"zzzzzzzzz, incorrecto.")
-        continue
-"""
